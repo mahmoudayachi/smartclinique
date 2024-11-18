759d0ec2f5094012f619692f61470b8bec1c86ab
mahmoudayachi <ayachimahmoud175@gmail.com>
2024-11-18 14:00:24 +0100

    first commit

---
+++ smartclinique/src/main/java/com/example/smartclinique/SmartcliniqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartclinique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcliniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcliniqueApplication.class, args);
+	}
+
+}
---
+++ smartclinique/src/test/java/com/example/smartclinique/SmartcliniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartclinique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcliniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
